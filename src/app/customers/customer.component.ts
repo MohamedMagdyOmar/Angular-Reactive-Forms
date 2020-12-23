b0ba5c5c9a92ea85b137321c5706dda61b8592fd
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-12-22 22:15:24 -0500

    Using SetValue And PatchValue

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -27,4 +27,13 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm));
   }
+
+  populateTestData(): void {
+    this.customerForm.setValue({
+      firstName: 'Mohamed',
+      lastName: 'Omar',
+      email: 'mohamed@gmail.com',
+      sendCatalog: true
+    })
+  }
 }
